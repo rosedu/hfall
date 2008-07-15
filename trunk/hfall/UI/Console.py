f4dbe78653be3a9bddfdb8480907acfb29edb12e
Sergiu Costea <nomail@rosedu.org>
2008-07-15 13:19:49 +0000

    fps counter

--- trunk/hfall/UI/Console.py
+++ trunk/hfall/UI/Console.py
@@ -13,45 +13,32 @@ sys.path.insert(0, "../Engine")
 import base
 import Render
 import UI
+from Sprite import Sprite
 
 class Console():
     """
-    The Console class. It represents one of the most important task in
+    The Console class. It represents one of the most important tasks in
     the game.
 
     """
     #Public variables
     enabled = False 
 		
-    def __init__(self):
+    def __init__(self,c_back,c_line,c_text):
         """
         About constructor.
         
         """
 	self.enabled = False
-        self.lines = []
-        pass
+	self.background = Sprite(0,UI.global_render.w.height-100,\
+	   UI.global_render.w.width,100,None,c_back) 
+	UI.global_UI.load_2DUI(self.background)
 
-    def name(self):
-        return 'Console'
+	#init background
+        pass
 
     def enable(self):
         self.enabled = True 
 
     def disable(self):
         self.enabled = False
-       
-    def put(self, text):
-	"""
-	Puts one line at the end of the lines list
-		
-	"""
-		
-	self.lines.push(text)
-				
-    def show_line(self, line):
-        """
-        Displays one line on the screens. Keep somehow track of displayed lines.
-
-        """
-        print line
