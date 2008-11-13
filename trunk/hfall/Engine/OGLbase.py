3f52dcee75a934a5fcb3ea538e87ab5228838888
Vali Priescu <nomail@rosedu.org>
2008-11-13 15:23:06 +0000

    ambient light

--- trunk/hfall/Engine/OGLbase.py
+++ trunk/hfall/Engine/OGLbase.py
@@ -296,7 +296,7 @@ class OGL:
 
     def configureMaterial(self, material):
         # Materials initialization and activation
-	#glMaterialfv (GL_FRONT_AND_BACK, GL_AMBIENT, material.ambient)
+	glMaterialfv (GL_FRONT_AND_BACK, GL_AMBIENT, material.ambient)
         #glMaterialfv (GL_FRONT_AND_BACK, GL_DIFFUSE, material.diffuse)
         #glMaterialfv (GL_FRONT_AND_BACK, GL_SPECULAR, material.specular)
         #glMaterialfv (GL_FRONT_AND_BACK, GL_SHININESS, material.shininess)
